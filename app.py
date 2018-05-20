6b8d079026940b80d8d9a447abdd3e62f569209d
Arpan Srivastava (Ubuntu) <arpansri98@gmail.com>
2018-05-20 10:40:02 +0530

    Updated the code according to PEP-8 Guidelines.

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import json
 import math
 import random
 import string
+import time
 from datetime import datetime
 
 import httplib2
 import requests
-import time
-from flask import Flask, render_template, request, flash, redirect, url_for, make_response, jsonify
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import make_response, jsonify
 from flask import session as login_session
 from oauth2client.client import flow_from_clientsecrets, FlowExchangeError
 from sqlalchemy import create_engine, asc, desc
@@ -26,10 +27,12 @@ Base.metadata.bind = engine
 session = scoped_session(sessionmaker(bind=engine))
 
 # Load up the client id from the client_secrets.json
-CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read()
+)['web']['client_id']
 
 # The current user object. Stores a dummy user if no one is logged in.
-user: User = None
+user = None
 
 
 # Flask Routes
@@ -50,13 +53,21 @@ def displayCatalog():
                 if request.form['picuri']:
                     picuri = request.form['picuri']
                 else:
-                    picuri = url_for('static', filename='img/athlete-beach-bodybuilder-305239.jpg')
-                userLocal = session.query(User).filter_by(email=user.email).one()
-                session.add(Category(name=category_name, picture=picuri, user=userLocal))
+                    picuri = url_for('static',
+                                     filename='img/athlete-beach-bodybuilder-305239.jpg')
+
+                userLocal = session \
+                    .query(User) \
+                    .filter_by(email=user.email) \
+                    .one()
+                session.add(Category(
+                    name=category_name, picture=picuri, user=userLocal)
+                )
                 session.commit()
                 return redirect(url_for('displayCatalog'))
             else:
-                flash("<big>Bummer! There's authentication error. Please try refreshing/logging in again.</big>")
+                flash(
+                    "<big>Bummer! There's authentication error. Please try refreshing/logging in again.</big>")
                 return redirect(url_for('displayCatalog'))
         else:
             flash(
@@ -74,7 +85,10 @@ def displayCategoryContents(catalog_name):
         if user is not None and user.id != 999:
             newItem = Item(
                 creationtime=datetime.now(),
-                category=session.query(Category).filter_by(name=catalog_name).one(),
+                category=session
+                .query(Category)
+                .filter_by(name=catalog_name)
+                .one(),
                 user=user)
             if request.form['name']:
                 newItem.name = request.form['name']
@@ -89,78 +103,127 @@ def displayCategoryContents(catalog_name):
             if request.form['picuri']:
                 newItem.picture = request.form['picuri']
             else:
-                newItem.picture = url_for('static', filename='img/athlete-beach-bodybuilder-305239.jpg')
+                newItem.picture = url_for('static',
+                                          filename='img/athlete-beach-bodybuilder-305239.jpg')
 
             session.add(newItem)
             session.commit()
             flash("Item {} created.".format(newItem.name))
         else:
             flash(
-                "<strong class='flash-message'>You are currently unauthorized to do this. Please <a href='{}'>sign in</a> to continue.</strong>".format(
-                    url_for('showLogin')))
-        return redirect(url_for('displayCategoryContents', catalog_name=catalog_name))
+                "<strong class='flash-message'>You are currently unauthorized to do this. Please <a href='{}'>sign in</a> to continue.</strong>"
+                .format(url_for('showLogin')))
+        return redirect(
+            url_for('displayCategoryContents',
+                    catalog_name=catalog_name)
+        )
 
     else:
         category = session.query(Category).filter_by(name=catalog_name).one()
         items = session.query(Item).filter_by(category=category)
-        return render_template('itemslist.html', items=items, catalog_name=catalog_name)
+        return render_template(
+            'itemslist.html',
+            items=items,
+            catalog_name=catalog_name
+        )
 
 
 # Displays the details of an item in a category
-@app.route('/catalog/<string:catalog_name>/<string:item_name>/', methods=['GET', 'POST'])
+@app.route('/catalog/<string:catalog_name>/<string:item_name>/',
+           methods=['GET', 'POST'])
 def displayItemDetails(catalog_name, item_name):
     if request.method == 'POST':
         # Check if the current user is not the dummy user
         if user is not None and user.id != 999:
-            editedItem: Item = session.query(Item).filter_by(name=item_name).one()
+            editedItem = session \
+                .query(Item) \
+                .filter_by(name=item_name) \
+                .one()
             if request.form['name']:
                 editedItem.name = request.form['name']
             if request.form['description']:
                 editedItem.description = request.form['description']
             if request.form['category']:
-                category = session.query(Category).filter_by(id=request.form['category']).one()
+                category = session \
+                    .query(Category) \
+                    .filter_by(id=request.form['category']) \
+                    .one()
                 editedItem.category = category
             session.add(editedItem)
             session.commit()
             flash('{} Successfully Edited'.format(editedItem.name))
-            return redirect(url_for('displayCategoryContents', catalog_name=catalog_name))
+            return redirect(url_for(
+                'displayCategoryContents',
+                catalog_name=catalog_name))
         else:
-            flash("<big>Authentication error occurred. Try refreshing the page/logging in again.</big>")
-            return redirect(url_for('displayCategoryContents', catalog_name=catalog_name))
+            flash(
+                "<big>Authentication error occurred. Try refreshing the page/logging in again.</big>"
+            )
+            return redirect(url_for(
+                'displayCategoryContents',
+                catalog_name=catalog_name
+            ))
 
     else:
         # Categories needed for editing the item details and displaying in drop down menu
         categories = session.query(Category).all()
-        catalog = session.query(Category).filter_by(name=catalog_name).one()
-        item = session.query(Item).filter_by(name=item_name, category=catalog).one()
+        catalog = session. \
+            query(Category). \
+            filter_by(name=catalog_name) \
+            .one()
+        item = session.query(Item) \
+            .filter_by(name=item_name, category=catalog) \
+            .one()
         itemslist = session.query(Item).filter_by(category=catalog)
-        return render_template('itemdetail.html', categories=categories, category=catalog, current_item=item,
-                               items=itemslist)
+        return render_template(
+            'itemdetail.html',
+            categories=categories,
+            category=catalog,
+            current_item=item,
+            items=itemslist
+        )
 
 
 # Displays the most recently added items
 @app.route('/recents/')
-def displayRecents():
+def displayRecent():
     categories = session.query(Category).order_by(asc(Category.name))
     recents = session.query(Item).order_by(desc(Item.creationtime))
-    return render_template('recent.html', categories=categories, recents=recents, current_time=datetime.now())
+    return render_template(
+        'recent.html',
+        categories=categories,
+        recents=recents,
+        current_time=datetime.now()
+    )
 
 
 # Route to delete a particular item in a category
-@app.route('/<string:category_name>/<string:item_name>/delete', methods=['GET', 'POST'])
+@app.route('/<string:category_name>/<string:item_name>/delete',
+           methods=['GET', 'POST'])
 def deleteItem(category_name, item_name):
     if request.method == 'POST':
         global user
         # Check if the current user is not the dummy user
         if user is not None and user.id != 999:
-            category = session.query(Category).filter_by(name=category_name).one()
-            session.delete(session.query(Item).filter_by(name=item_name, category=category).one())
+            category = session \
+                .query(Category) \
+                .filter_by(name=category_name) \
+                .one()
+            session.delete(session
+                           .query(Item)
+                           .filter_by(name=item_name, category=category)
+                           .one()
+                           )
             session.commit()
         else:
             flash(
-                "<strong class='flash-message'>You are currently unauthorized to do this. Please <a href='{}'>sign in</a> to continue.</strong>".format(
-                    url_for('showLogin')))
-        return redirect(url_for('displayItemDetails', catalog_name=category_name, item_name=item_name))
+                "<strong class='flash-message'>You are currently unauthorized to do this. Please <a href='{}'>sign in</a> to continue.</strong>"
+                .format(url_for('showLogin')))
+        return redirect(url_for(
+            'displayItemDetails',
+            catalog_name=category_name,
+            item_name=item_name
+        ))
     else:
         return "Sorry! We don't accept 'GET' requests. :/"
 
@@ -171,15 +234,18 @@ def deleteCategory(category_name):
     if request.method == 'POST':
         # Check if the current user is not the dummy user
         if user is not None and user.id != 999:
-            category = session.query(Category).filter_by(name=category_name).one()
+            category = session \
+                .query(Category) \
+                .filter_by(name=category_name) \
+                .one()
             for item in session.query(Item).filter_by(category=category).all():
                 session.delete(item)
             session.delete(category)
             session.commit()
         else:
             flash(
-                "<strong class='flash-message'>You are currently unauthorized to do this. Please <a href='{}'>sign in</a> to continue.</strong>".format(
-                    url_for('showLogin')))
+                "<strong class='flash-message'>You are currently unauthorized to do this. Please <a href='{}'>sign in</a> to continue.</strong>"
+                .format(url_for('showLogin')))
         return redirect(url_for('displayCatalog'))
     else:
         return "Sorry! We don't accept 'GET' requests. :/"
@@ -191,7 +257,9 @@ def deleteCategory(category_name):
 def showLogin():
     updateUser()
     # Create a random state key to pass in the login.html
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in range(32)
+                    )
     login_session['state'] = state
     return render_template('login.html', STATE=state, user=user)
 
@@ -210,12 +278,17 @@ def gconnect():
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
-        response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
+        response = make_response(json.dumps(
+            'Failed to upgrade the authorization code.'), 401
+        )
         response.headers['Content-Type'] = 'application/json'
         return response
 
     access_token = credentials.access_token
-    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    url = (
+            'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+            % access_token
+    )
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
 
@@ -227,7 +300,9 @@ def gconnect():
     # Token's user ID doesn't match given user ID
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
-        response = make_response(json.dumps("Token's user ID doesn't match given user ID"), 401)
+        response = make_response(json.dumps(
+            "Token's user ID doesn't match given user ID"), 401
+        )
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -242,7 +317,9 @@ def gconnect():
     stored_gplus_id = login_session.get('gplus_id')
 
     if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'), 200)
+        response = make_response(json.dumps(
+            'Current user is already connected.'), 200
+        )
         response.headers['Content-Type'] = 'application/json'
 
     login_session['credentials'] = credentials.to_json()
@@ -268,7 +345,8 @@ def gconnect():
     login_session['user_id'] = user_id
     updateUser()
 
-    flash("<big>You are now logged in as {}</big>".format(login_session['username']))
+    flash("<big>You are now logged in as {}</big>"
+          .format(login_session['username']))
     return redirect(url_for('displayCatalog'))
 
 
@@ -286,7 +364,8 @@ def gdisconnect():
         response = make_response(json.dumps('Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % credentials['access_token']
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % credentials[
+        'access_token']
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
     print('result is ')
@@ -304,7 +383,9 @@ def gdisconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     else:
-        response = make_response(json.dumps('Failed to revoke token for given user.'))
+        response = make_response(json.dumps(
+            'Failed to revoke token for given user.')
+        )
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -316,8 +397,16 @@ def catalogJSON():
     categories = session.query(Category).all()
     category_dict = [c.serialize for c in categories]
     for c in range(len(category_dict)):
-        category = session.query(Category).filter_by(name=category_dict[c]["name"]).one()
-        items = [i.serialize for i in session.query(Item).filter_by(category=category).all()]
+        category = session \
+            .query(Category) \
+            .filter_by(name=category_dict[c]["name"]) \
+            .one()
+        items = [
+            i.serialize for i in session
+            .query(Item)
+            .filter_by(category=category)
+            .all()
+        ]
         if items:
             category_dict[c]["items"] = items
 
@@ -336,7 +425,8 @@ def categoryContentsJSON(catalog_name):
 @app.route('/catalog/<string:category_name>/<string:item_name>/JSON')
 def itemJSON(category_name, item_name):
     category = session.query(Category).filter_by(name=category_name).one()
-    item = session.query(Item).filter_by(name=item_name, category=category).one()
+    item = session.query(Item).filter_by(name=item_name,
+                                         category=category).one()
     return jsonify(item=item.serialize)
 
 
@@ -348,17 +438,25 @@ def updateUser():
         # Find user by email
         user = session.query(User).filter_by(email=login_session['email']).one()
     except:
-        # If user doesn't exist, create a dummy user and set it to global user variable
-        user = User(id=999, name="Please Log In", picture=url_for('static', filename="img/profile.png"))
+        # Create dummy user
+        user = User(
+            id=999,
+            name="Please Log In",
+            picture=url_for('static', filename="img/profile.png")
+        )
 
 
 # Creates a user from the login_session object
 def createUser(login_session):
-    newUser = User(name=login_session['username'], email=login_session['email'], picture=login_session['picture'])
+    newUser = User(
+        name=login_session['username'],
+        email=login_session['email'],
+        picture=login_session['picture'])
     session.add(newUser)
     session.commit()
     user = session.query(User).filter_by(email=login_session['email']).one()
     return user.id
+
 
 # Returns the user_id from the email
 def getUserID(email):
